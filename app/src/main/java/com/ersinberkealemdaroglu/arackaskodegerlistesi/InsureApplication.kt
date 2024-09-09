76b36d65df44833f20bd383976488d6e07f88ee4
EXT02D52124 <ersin.alemdaroglu@consultant.turkcell.com.tr>
2024-09-09 16:49:33 +0300

    crashlytics eklendi.

--- app/src/main/java/com/ersinberkealemdaroglu/arackaskodegerlistesi/InsureApplication.kt
+++ app/src/main/java/com/ersinberkealemdaroglu/arackaskodegerlistesi/InsureApplication.kt
@@ -2,6 +2,7 @@ package com.ersinberkealemdaroglu.arackaskodegerlistesi
 
 import android.app.Application
 import android.content.Context
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,6 +11,8 @@ class InsureApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+
     }
 
     companion object {
